92e28079d4cfaa57590295489a89f8d3b30f8a66
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-16 07:28:04 -0400

    Add files via upload

---
+++ Modul 2/Soal 9/PRAK109-2210817120012-Noorlaini Marratain.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int pasYZ,pah,dikalahkan;
+    pasYZ = 958730;
+    pah = 5;
+    dikalahkan = pasYZ/pah;
+    printf("Jumlah pasukan yang dibawa Yu Zhong = %d\n",pasYZ);
+    printf("Jumlah pahlawan = %d\n",pah);
+    printf("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah %d pasukan\n",dikalahkan);
+    return 0;
+}
---
+++ Modul 2/Soal 9/PRAK109-2210817120012-Noorlaini Marratain.py
@@ -0,0 +1,6 @@
+pasYZ = 958730
+pah = 5
+dikalahkan = pasYZ/pah
+print("Jumlah pasukan yang dibawa Yu Zhong = ",pasYZ)
+print("Jumlah pahlawan = ",pah)
+print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah ",round(dikalahkan),"pasukan")
