ab44ebbcc378691f4180c183a57d61298a29ab9a
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-16 07:35:13 -0400

    Add files via upload

---
+++ Modul 2/Soal 1/PRAK101-2210817120012-Noorlaini Marratain.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+    int x, y, z, jumlah_variabel;
+    x=5, y=7, z=9;
+    jumlah_variabel=x+y+z;
+    printf("Variabel x bernilai %d\n" ,x);
+    printf("Variabel y bernilai %d\n" ,y);
+    printf("Variabel z bernilai %d\n" ,z);
+    printf("Jumlah variabel tersebut adalah %d\n" ,jumlah_variabel);
+    return 0;
+}
---
+++ Modul 2/Soal 1/PRAK101-2210817120012-Noorlaini Marratain.py
@@ -0,0 +1,8 @@
+x = 5
+y = 7
+z = 9
+jumlah_variabel=x+y+z
+print("Variabel x bernilai" ,x)
+print("Variabel y bernilai" ,y)
+print("Variabel z bernilai" ,z)
+print("Jumlah variabel tersebut adalah" ,jumlah_variabel)
