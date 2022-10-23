f92b6fd4d641a9ddd85ea39fe10f20d064a8aa01
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-23 08:38:51 -0400

    Add files via upload

--- Modul 3/Soal 3/PRAK203-2210817120012-Noorlaini Marratain.c
+++ Modul 3/Soal 3/PRAK203-2210817120012-Noorlaini Marratain.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    for (int i=0; i<=1; i++){
+    float a,b,i,j,x,y, hasil;
+    scanf("%f %f", &a,&b);
+    scanf("%f %f", &i,&j);
+    scanf("%f %f", &x,&y);
+    hasil =(a-b)*i/j-(x+y);
+    printf("%.3f\n\n",hasil);
+    }
+}
