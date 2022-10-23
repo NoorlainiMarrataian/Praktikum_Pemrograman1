ffcc33a0ad313ac8eda84c9e43be1ad93fef4cc1
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-23 08:37:45 -0400

    Add files via upload

--- Modul 3/Soal 2/PRAK202-2210817120012-Noorlaini Marratain.c
+++ Modul 3/Soal 2/PRAK202-2210817120012-Noorlaini Marratain.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main()
+{
+    for (int i=0; i<=1; i++){
+    float np,nk,hasil;
+    printf("Masukkan Nilai Pertama: ");
+    scanf("%g", &np);
+    printf("Masukkan Nilai Kedua: ");
+    scanf("%g", &nk);
+    hasil=np+nk;
+    printf("Hasil dari penjumlahan nilai pertama \"%g\" dan nilai kedua \"%g\" adalah \"%g\"\n\n", np, nk, hasil);
+}
+}
---
+++ Modul 3/Soal 2/PRAK202-2210817120012-Noorlaini Marratain.py
@@ -0,0 +1,8 @@
+np = float(input("Masukkan Nilai Pertama: "))
+nk = float(input("Masukkan Nilai Kedua: "))
+hasil = np+nk
+print("Hasil dari penjumlahan nilai pertama \"%.0f"%np,"\" dan nilai kedua \"",nk,"\" adalah \"%.2f" %hasil,"\"\n")
+np1  = float(input("Masukkan Nilai Pertama: "))
+nk1 = float(input("Masukkan Nilai Kedua: "))
+hasil1 = np1+nk1
+print("Hasil dari penjumlahan nilai pertama \"%.2f" %np1,"\" dan nilai kedua \"",nk1,"\" adalah \"%.2f" %hasil1,"\"\n")
