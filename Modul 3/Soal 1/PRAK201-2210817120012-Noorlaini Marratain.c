16d44437da3d3e497e97fb638996bc33743a5dc9
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-23 08:35:42 -0400

    Add files via upload

--- Modul 3/Soal 1/PRAK201-2210817120012-Noorlaini Marratain.c
+++ Modul 3/Soal 1/PRAK201-2210817120012-Noorlaini Marratain.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main()
+{
+    char na[19], ni[13], k[1], ttl[18], a[39], h[7], no[12];
+
+    printf("Nama                :");
+    gets(na);
+    printf("NIM                 :");
+    gets(ni);
+    printf("Kelas Paralel       :");
+    gets(k);
+    printf("Tempat/Tanggal Lahir:");
+    gets(ttl);
+    printf("Alamat              :");
+    gets(a);
+    printf("Hobby               :");
+    gets(h);
+    printf("No. HP              :");
+    gets(no);
+    return 0;
+}
