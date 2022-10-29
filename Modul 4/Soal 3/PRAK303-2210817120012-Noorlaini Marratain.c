b9f24b027346ad8947c4f087de071a77075df68c
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-29 06:22:46 -0400

    Add files via upload

--- Modul 4/Soal 3/PRAK303-2210817120012-Noorlaini Marratain.c
+++ Modul 4/Soal 3/PRAK303-2210817120012-Noorlaini Marratain.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main()
+{
+    int bb;
+    scanf("%d", &bb);
+    if(bb>0){
+        printf("positif\n");}
+    else if(bb<0){
+        printf("negatif\n");}
+    else{
+        printf("nol\n");}
+}
---
+++ Modul 4/Soal 3/PRAK303-2210817120012-Noorlaini Marratain.py
@@ -0,0 +1,7 @@
+bb=int(input())
+if (bb>0):
+    print("positif\n")
+elif(bb<0):
+    print("negatif\n")
+else:
+    print("nol\n")
