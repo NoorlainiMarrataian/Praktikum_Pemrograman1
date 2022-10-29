9f0d93fa8c588ed96e27f0c2a664f28f8d0dec50
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-10-29 06:21:21 -0400

    Add files via upload

--- Modul 4/Soal 2/PRAK302-2210817120012-Noorlaini Marratain.c
+++ Modul 4/Soal 2/PRAK302-2210817120012-Noorlaini Marratain.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+    int N;
+    scanf("%i", &N);
+    if (N>=80 && N<=100){
+        printf("A\n", N);}
+    else if(N>70 && N<=79){
+        printf("B\n");}
+    else if(N>=60 && N<=69){
+        printf("C\n");}
+    else if(N>=50 && N<=59){
+        printf("D\n");}
+    else if(N<50){
+        printf("E\n");}
+}
---
+++ Modul 4/Soal 2/PRAK302-2210817120012-Noorlaini Marratain.py
@@ -0,0 +1,11 @@
+N=int(input())
+if(N>=80 and N<=100):
+    print("A\n")
+elif(N>=70 and N<=79):
+    print("B\n") 
+elif(N>=60 and N<=69):
+    print("C\n")
+elif(N>=50 and N<=59):
+    print("D\n")
+else:
+    print("E\n")
