6a27e322ad0daf9cff3c1648447f44d30c709bc6
NoorlainiMarrataian <115251206+NoorlainiMarrataian@users.noreply.github.com>
2022-11-20 18:04:05 -0500

    Add files via upload

--- Modul 7/Soal 3/PRAK603-2210817120012-Noorlaini Marratain.c
+++ Modul 7/Soal 3/PRAK603-2210817120012-Noorlaini Marratain.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main()
+{
+    int b1, b2, isi;
+    scanf("%d %d", &b1, &b2);
+    int angka1[b1], angka2[b2];
+    if(b1 != b2){
+        printf("Jumlah tidak sama");
+    }else{
+        for(int i=0; i<b1; i++){
+            scanf("%d", &isi);
+            angka1[i]=isi;
+        }
+        for(int i=0; i<b2; i++){
+            scanf("%d", &isi);
+            angka2[i]=isi;
+    }
+    printf("\n");
+    for(int i=0; i<b2; i++){
+        printf("%d ", angka1[i]*angka2[i]);
+        }
+    }
+return 0;
+}
